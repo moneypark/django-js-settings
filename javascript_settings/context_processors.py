83e246ed23bf9f0403ee068603c0a4277d0375e0
Vasyl Dizhak <dijakroot@gmail.com>
2016-01-19 15:26:24 +0100

    Fix encoding of Decimal numbers using DjangoJSONEncoder class

--- javascript_settings/context_processors.py
+++ javascript_settings/context_processors.py
@@ -1,6 +1,8 @@
 import json
 from configuration_builder import DEFAULT_CONFIGURATION_BUILDER
 
+from django.core.serializers.json import DjangoJSONEncoder
+
 
 def get_config(request):
-    return { 'javascript_settings': json.dumps(DEFAULT_CONFIGURATION_BUILDER.get_configuration(request)) }
+    return { 'javascript_settings': json.dumps(DEFAULT_CONFIGURATION_BUILDER.get_configuration(request), cls=DjangoJSONEncoder) }
